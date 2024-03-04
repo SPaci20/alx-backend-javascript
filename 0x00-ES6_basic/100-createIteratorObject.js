40afd2b83a5de75e6d41a13eab9813111350cacb
SPaci20 <spaci20@gmail.com>
2024-03-04 15:57:34 -0800

    updates

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,39 @@
 export default function createIteratorObject(report) {
-  while (report) {
-    console.log('Placeholder');
-  }
+  const iterator = {
+    departments: Object.keys(report),
+    employees: {},
+    currentDepartmentIndex: 0,
+    currentEmployeeIndex: 0,
+
+    next() {
+      const currentDepartment = this.departments[this.currentDepartmentIndex];
+
+      if (!currentDepartment) {
+        return { done: true };
+      }
+
+      const currentEmployees = this.employees[currentDepartment];
+
+      if (!currentEmployees) {
+        this.employees[currentDepartment] = Object.values(report[currentDepartment]);
+      }
+
+      const currentEmployee = this.employees[currentDepartment][this.currentEmployeeIndex];
+
+      if (this.currentEmployeeIndex < currentEmployees.length - 1) {
+        this.currentEmployeeIndex++;
+      } else {
+        this.currentEmployeeIndex = 0;
+        this.currentDepartmentIndex++;
+      }
+
+      return { value: currentEmployee, done: false };
+    },
+  };
+
+  iterator[Symbol.iterator] = function () {
+    return this;
+  };
+
+  return iterator;
 }
